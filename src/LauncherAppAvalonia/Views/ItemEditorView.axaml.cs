5b17227b9377471529ca4b61d4aa55091871a87d
ZQY <VDerGoW@hotmail.com>
2025-07-06 14:52:53 +0800

    feat: ItemEditorView

---
+++ src/LauncherAppAvalonia/Views/ItemEditorView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LauncherAppAvalonia.Views;
+
+public partial class ItemEditorView : UserControl
+{
+    public ItemEditorView()
+    {
+        InitializeComponent();
+    }
+}
