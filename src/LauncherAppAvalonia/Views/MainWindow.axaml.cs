51500bbcd264f00babe347ea3618c106634e6afd
ZQY <VDerGoW@hotmail.com>
2025-07-12 14:39:40 +0800

    double tap to open item

--- src/LauncherAppAvalonia/Views/MainWindow.axaml.cs
+++ src/LauncherAppAvalonia/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using LauncherAppAvalonia.ViewModels;
 
 namespace LauncherAppAvalonia.Views;
 
@@ -7,5 +9,15 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnItemListBoxItemDoubleTapped(object? sender, TappedEventArgs e)
+    {
+        LauncherItemViewModel? itemVM = (sender as ListBox)?.SelectedItem as LauncherItemViewModel;
+        if (itemVM == null)
+            return;
+
+        if (DataContext is MainWindowViewModel viewModel)
+            viewModel.OpenItemCommand.Execute(itemVM);
     }
 }
